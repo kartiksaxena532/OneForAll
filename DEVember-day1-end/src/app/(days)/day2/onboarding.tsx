76bc50b641b6564f92ad7be52b6991b79b7c9066
Kartik Saxena <karrtiksaxena@gmail.com>
2023-12-22 17:05:51 +0530

    onboarding1

--- DEVember-day1-end/src/app/(days)/day2/onboarding.tsx
+++ DEVember-day1-end/src/app/(days)/day2/onboarding.tsx
@@ -1,17 +1,26 @@
-import {View, Text,StyleSheet ,Image} from "react-native";
+import {View, Text,StyleSheet ,Image,Pressable} from "react-native";
 import { Stack } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 
 
 export default function OnboardingScreen(){
 
 return (
-<View style= {styles.container}>
+<SafeAreaView style= {styles.container}>
     <Stack.Screen options={{headerShown : false}}/>
-    <Image source={require('../day2/images2/geek.png')}/>
+    <View>
+    <Image style={styles.image} source={require('../day2/images2/nerd.png')}/>
 <Text style= {styles.title}>Hello! Mr.Kartik</Text>
 <Text style= {styles.description}>Which persona would you like to choose today? On this onboarding screen you can monitor your everyday persona and help others to get overwhelmed by the fact of your react native skills. Have fun!</Text>
 </View>
+<View style={styles.buttonRow}>
+    <Pressable style={styles.button}>
+    <Text style={styles.buttonText}> Nerdy
+    </Text>
+    </Pressable>
+</View>
+</SafeAreaView>
 )
 
 }
@@ -25,24 +34,47 @@ const styles = StyleSheet.create({
     title:{
         color:'#FDFDFD',
         fontFamily:'Inter',
-        fontSize:28,
+        fontSize:40,
         letterSpacing:1.5,
-
+        textAlign:'left',
         padding:20,
+        
 
     },
     image:{
-
+        alignSelf: "center",
+        
 
     },
     description:{
 
         color:'grey',
         fontFamily:'InterSemi',
-        fontSize:18,
+        fontSize:19,
        textAlign:'left',
        padding:10,
+       paddingHorizontal:20,
+       lineHeight:28 ,
 
 
     },
+    buttonText:{
+        fontSize:20,
+        color:'#FDFDFD',
+
+
+    },
+    buttonRow:{
+
+
+    },
+    button:{
+
+
+    },
+    
+
+    
+
+
   });
